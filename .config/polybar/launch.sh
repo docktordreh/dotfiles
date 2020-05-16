e5347740ee6b5e54898cfc37d7958b9845de20e6
Valentin Lechner <valentin_lechner@dismail.de>
2020-05-16 19:50:54 +0200

    Committing changes after long time
    .Xresources:
    * Cursor is now visible when on terminals
    * Changed color8
    * Now using Fira Mono as font and as fallback font inconsolata
    * Also the cursor blinks
    .dunstrc:
    * major updates, changed design a lot
    i3/config:
    * Changed font
    * Using st as default term

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,8 +8,16 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
 # Bar is the name set in the polybar config, so if you change it, you haveto change it here too.
-polybar main &
+polybar top &
 polybar bottom &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload main &
+  polybar --reload bottom &
+fi
 
 echo "Bars launched..."
-
