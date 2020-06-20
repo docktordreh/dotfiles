192aa733c0e43159528741a1affa173f48377a0c
valentin_lechner <valentin_lechner@dismail.de>
2020-06-20 23:06:02 +0200

    Changin setup layout

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 tmux start-server
 tmux new-session \; \
-    split-window -v -p 70 \; \
-    split-window -h -p 40 \; \
     send-keys 'vifm' C-m \; \
-    select-pane -t 1 \; \
+    split-window -v -p 70 \; \
     new-window 'neomutt' \; \
     new-window 'ncmpcpp' \; \
-    new-window 'asciiquarium'; 
+    new-window 'asciiquarium' \; \
+    select-window -t 0 \; \
+    select-pane -t 1; 
