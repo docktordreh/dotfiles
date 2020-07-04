fbcb49341e3b3413344917083532831ee6e26306
valentin_lechner <valentin_lechner@dismail.de>
2020-07-04 20:32:18 +0200

    doom: install jinja2-mode for ansi-templates

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -12,6 +12,7 @@
 (package! beacon)
 (package! typo)
 (package! ob-async)
+(package! jinja2-mode)
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
 ;(unpin! pinned-package)
