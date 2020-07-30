3b44dde1d8c5caff2397ae72fd26f916219b73e4
valentin_lechner <valentin_lechner@dismail.de>
2020-07-30 19:05:38 +0200

    doom: update config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -26,7 +26,106 @@
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*My defaults][My defaults:5]]
 (custom-set-variables
- '(zoom-mode t))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#222323" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
+ '(custom-safe-themes
+   (quote
+    ("54cf3f8314ce89c4d7e20ae52f7ff0739efb458f4326a2ca075bf34bc0b4f499" "5d145f3b18070a7c263945f743811693d89e9fe86f50d1a638cd89467e0551fe" "272938f764232c32ab4c5139b4091b3fd1ea72765d3872dd7beb3c72703d9e8c" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" default)))
+ '(fci-rule-color "#e2e4e5")
+ '(flymake-error-bitmap
+   (quote
+    (flymake-double-exclamation-mark modus-theme-fringe-red)))
+ '(flymake-note-bitmap (quote (exclamation-mark modus-theme-fringe-cyan)))
+ '(flymake-warning-bitmap (quote (exclamation-mark modus-theme-fringe-yellow)))
+ '(highlight-tail-colors (quote (("#2f4a00" . 0) ("#00415e" . 20))))
+ '(hl-todo-keyword-faces
+   (quote
+    (("HOLD" . "#cfdf30")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae8fc")
+     ("DONT" . "#80d200")
+     ("FAIL" . "#ff8059")
+     ("BUG" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#f0ce43")
+     ("KLUDGE" . "#eecc00")
+     ("HACK" . "#eecc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9977")
+     ("XXX+" . "#f4923b")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#bfd9ff"))))
+ '(ibuffer-deletion-face (quote modus-theme-mark-del))
+ '(ibuffer-filter-group-name-face (quote modus-theme-mark-symbol))
+ '(ibuffer-marked-face (quote modus-theme-mark-sel))
+ '(ibuffer-title-face (quote modus-theme-header))
+ '(jdee-db-active-breakpoint-face-colors (cons "#282a36" "#57c7ff"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#282a36" "#5af78e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#282a36" "#848688"))
+ '(objed-cursor-color "#ff5c57")
+ '(package-selected-packages
+   (quote
+    (modus-vivendi-theme org-ref auto-dim-other-buffers counsel-tramp sos org-super-agenda org-chef org-bullets beacon avy)))
+ '(pdf-view-midnight-colors (cons "#f9f9f9" "#282a36"))
+ '(rustic-ansi-faces
+   ["#282a36" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
+ '(safe-local-variable-values
+   (quote
+    ((eval add-hook
+           (quote after-save-hook)
+           (lambda nil
+             (org-babel-tangle))
+           nil t)
+     (org-export-allow-bind-keywords . t))))
+ '(vc-annotate-background "#282a36")
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#5af78e")
+    (cons 40 "#8df793")
+    (cons 60 "#c0f898")
+    (cons 80 "#f3f99d")
+    (cons 100 "#f7e38c")
+    (cons 120 "#fbcd7c")
+    (cons 140 "#ffb86c")
+    (cons 160 "#ff9e88")
+    (cons 180 "#ff84a4")
+    (cons 200 "#ff6ac1")
+    (cons 220 "#ff659d")
+    (cons 240 "#ff607a")
+    (cons 260 "#ff5c57")
+    (cons 280 "#e06663")
+    (cons 300 "#c1716f")
+    (cons 320 "#a27b7b")
+    (cons 340 "#e2e4e5")
+    (cons 360 "#e2e4e5")))
+ '(vc-annotate-very-old-color nil)
+ '(xterm-color-names
+   ["#000000" "#ff8059" "#44bc44" "#eecc00" "#29aeff" "#feacd0" "#00d3d0" "#a8a8a8"])
+ '(xterm-color-names-bright
+   ["#181a20" "#f4923b" "#80d200" "#cfdf30" "#72a4ff" "#f78fe7" "#4ae8fc" "#ffffff"])
+ '(zoom-ignore-predicates
+   (quote
+    ((lambda nil
+       (>
+        (count-lines
+         (point-min)
+         (point-max))
+        20)))))
+ '(zoom-ignored-buffer-name-regexps (quote ("^*calc")))
+ '(zoom-ignored-buffer-names (quote ("zoom.el" "init.el")))
+ '(zoom-ignored-major-modes (quote (dired-mode markdown-mode treemacs-mode)))
+ '(zoom-mode t t)
+ '(zoom-size (quote (0.618 . 0.618))))
 ;; My defaults:5 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*My defaults][My defaults:6]]
@@ -375,95 +474,39 @@
 ;; Look and feel:7 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:8]]
-(add-hook 'text-mode-hook #'flyspell-mode)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "#57c7ff"))))
+ '(doom-modeline-evil-insert-state ((t (:weight bold :foreground "#339CDB"))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark violet"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#57c7ff" :height 2.0 :underline nil))))
+ '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#5af78e" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#ff6ac1" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#f3f99d" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#9aeedf" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#ff5c57"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#57c7ff"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#5af78e"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#ff6ac1"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:foreground "cyan"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
 ;; Look and feel:8 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:9]]
-(custom-set-faces
-  '(org-block
-    ((t (:inherit fixed-pitch)))
-    )
-  '(org-code
-    ((t (:inherit (shadow fixed-pitch))))
-    )
-  '(org-document-info
-    ((t (:foreground "dark violet")))
-    )
-  '(org-document-info-keyword
-    ((t (:inherit (shadow fixed-pitch))))
-    )
-  '(org-indent
-    ((t (:inherit (org-hide fixed-pitch))))
-    )
-  '(org-link
-    ((t (:foreground "royal blue" :underline t)))
-    )
-  '(org-meta-line
-    ((t (:inherit (font-lock-comment-face fixed-pitch))))
-    )
-  '(org-property-value
-    ((t (:inherit fixed-pitch)))
-    t)
-  '(org-special-keyword
-    ((t (:inherit (font-lock-comment-face fixed-pitch))))
-    )
-  '(org-table
-    ((t ( :foreground "cyan")))
-              )
-  '(org-tag
-    ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8)))
-    )
-  '(org-verbatim
-    ((t (:inherit (shadow fixed-pitch))))
-    )
-  '(org-level-8 ((t (
-                      :inherit default
-                      :weight bold
-                      :foreground "#ff6ac1"
-                      ))))
-  '(org-level-7 ((t (
-                     :inherit default
-                     :weight bold
-                     :foreground "#5af78e"))))
-  '(org-level-6 ((t (
-                     :inherit default
-                     :weight bold
-                     :foreground "#57c7ff"
-                     ))))
-  '(org-level-5 ((t (
-                     :inherit default
-                     :weight bold
-                     :foreground "#ff5c57"
-                     ))))
-  '(org-level-4 ((t (
-                     :inherit default
-                     :weight bold
-                     :foreground "#9aeedf"
-                     :height 1.1))))
-   '(org-level-3 ((t (
-                      :inherit default
-                      :weight bold
-                      :foreground "#f3f99d"
-                      :height 1.25))))
-   '(org-level-2 ((t (
-                      :inherit default
-                      :weight bold
-                      :foreground "#ff6ac1"
-                      :height 1.5))))
-   '(org-level-1 ((t (
-                      :inherit default
-                      :weight bold
-                      :foreground "#5af78e"
-                      :height 1.75))))
-   '(org-document-title ((t (
-                             :inherit default
-                             :weight bold
-                             :foreground "#57c7ff"
-                             :height 2.0
-                             :underline nil)))))
-;; Look and feel:9 ends here
-
-;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:10]]
 (font-lock-add-keywords 'org-mode
                         '(("^ *\\([-]\\) "
                            (0 (prog1 ()
@@ -508,23 +551,23 @@
  org-fontify-whole-heading-line t
  org-fontify-done-headline t
  org-fontify-quote-and-verse-blocks t)
+;; Look and feel:12 ends here
+
+;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:13]]
+(add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
+(add-hook 'org-mode-hook 'auto-fill-mode)
 ;; Look and feel:13 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:14]]
-(add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
-(add-hook 'org-mode-hook 'auto-fill-mode)
-;; Look and feel:14 ends here
-
-;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:15]]
 (setq org-enforce-todo-dependencies t)
 (setq org-insert-heading-respect-content nil)
 (setq org-reverse-note-order nil)
 (setq org-deadline-warning-days 7)
 (setq org-blank-before-new-entry (quote ((heading . t)
                                          (plain-list-item . nil))))
-;; Look and feel:15 ends here
+;; Look and feel:14 ends here
 
-;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:16]]
+;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Look and feel][Look and feel:15]]
 (after! org
   (defun unpackaged/org-element-descendant-of (type element)
     "Return non-nil if ELEMENT is a descendant of TYPE.
@@ -810,13 +853,7 @@ appropriate.  In tables, insert a new row or end the table."
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Agenda][Agenda:7]]
 (setq org-fontify-done-headline t)
-(custom-set-faces
- '(org-done ((t (:foreground "PaleGreen"
-                 :weight normal
-                 :strike-through t))))
- '(org-headline-done
-   ((((class color) (min-colors 16) (background dark))
-     (:foreground "LightSalmon" :strike-through t)))))
+
 ;; Agenda:7 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Agenda][Agenda:8]]
@@ -2340,8 +2377,7 @@ CONTENTS is nil.  INFO is a plist holding contextual information."
 ;; Extra links:1 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Zoom][Zoom:1]]
-(custom-set-variables
- '(zoom-size '(0.618 . 0.618)))
+
 ;; Zoom:1 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Zoom][Zoom:2]]
@@ -2349,27 +2385,7 @@ CONTENTS is nil.  INFO is a plist holding contextual information."
 ;; Zoom:2 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Zoom][Zoom:3]]
-(custom-set-variables
- '(zoom-ignored-major-modes
-   '(
-     dired-mode
-     markdown-mode
-     treemacs-mode
-     ))
-'(zoom-ignored-buffer-names
-  '(
-    "zoom.el"
-    "init.el"
-    )
-  )
- '(zoom-ignored-buffer-name-regexps '("^*calc"))
- '(zoom-ignore-predicates
-   '((lambda () (
-                 > (count-lines (point-min) (point-max)) 20)
-      )
-    )
-  )
-)
+
 ;; Zoom:3 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Keycast][Keycast:1]]
@@ -2455,55 +2471,5 @@ Return output file name."
 ;; Bachelors thesis:1 ends here
 
 ;; [[file:../Projekte/dotfiles/.doom.d/config.org::*Variables I accept][Variables I accept:1]]
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#191a2a" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
- '(custom-safe-themes
-   (quote
-    ("bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" default)))
- '(fci-rule-color "#e2e4e5")
- '(jdee-db-active-breakpoint-face-colors (cons "#282a36" "#57c7ff"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#282a36" "#5af78e"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#282a36" "#848688"))
- '(objed-cursor-color "#ff5c57")
- '(package-selected-packages
-   (quote
-    (org-ref auto-dim-other-buffers counsel-tramp sos org-super-agenda org-chef org-bullets beacon avy)))
- '(pdf-view-midnight-colors (cons "#f9f9f9" "#282a36"))
- '(rustic-ansi-faces
-   ["#282a36" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
- '(safe-local-variable-values
-   (quote
-    ((eval add-hook
-           (quote after-save-hook)
-           (lambda nil
-             (org-babel-tangle))
-           nil t)
-     (org-export-allow-bind-keywords . t))))
- '(vc-annotate-background "#282a36")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#5af78e")
-    (cons 40 "#8df793")
-    (cons 60 "#c0f898")
-    (cons 80 "#f3f99d")
-    (cons 100 "#f7e38c")
-    (cons 120 "#fbcd7c")
-    (cons 140 "#ffb86c")
-    (cons 160 "#ff9e88")
-    (cons 180 "#ff84a4")
-    (cons 200 "#ff6ac1")
-    (cons 220 "#ff659d")
-    (cons 240 "#ff607a")
-    (cons 260 "#ff5c57")
-    (cons 280 "#e06663")
-    (cons 300 "#c1716f")
-    (cons 320 "#a27b7b")
-    (cons 340 "#e2e4e5")
-    (cons 360 "#e2e4e5")))
- '(vc-annotate-very-old-color nil))
+
 ;; Variables I accept:1 ends here
