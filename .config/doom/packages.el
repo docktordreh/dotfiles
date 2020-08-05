2edfa963d11f5f199d694b3ece1a0ea47a25603b
valentin_lechner <valentin_lechner@dismail.de>
2020-08-05 17:59:48 +0200

    Doom config update
    config: Removing cyan for org-tables
    
    Adding calendar lat and lon for Freiburg
    -> change theme depending on time of day using theme-changer
    packages: Adding theme-changer
    
    Removing theme-magic-from emacs since the desktop looks bad in white imo (especially the bar for me
    removing setq doom-theme because it interfered with change-theme
    org: changing colors
    
    packages: correcting syntax error
    removing bachelors thesis
    
    init: removing doom-quit, don't need prompt to kill emacs
    
    removing haskell
    removing lua
    removing nix
    removing ruby

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,74 +1,82 @@
 ;; -*- no-byte-compile: t; -*-
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:1]]
-package! org-pretty-table-mode
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:1]]
+(package! org-pretty-table-mode
   :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "88380f865a...")
 ;; Additional Org Packages:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:2]]
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:2]]
 (package! org-super-agenda)
 ;; Additional Org Packages:2 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:3]]
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:3]]
 (package! doct
   :recipe (:host github :repo "progfolio/doct"))
 ;; Additional Org Packages:3 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:4]]
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:4]]
 (package! org-fragtog)
 ;; Additional Org Packages:4 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:5]]
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:5]]
 (package! org-pretty-tags)
 ;; Additional Org Packages:5 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:6]]
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:6]]
 (package! org-chef)
 ;; Additional Org Packages:6 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Additional Org Packages][Additional Org Packages:7]]
+;; [[file:/tmp/config.org.YCVx5j::*Additional%20Org%20Packages][Additional Org Packages:7]]
 (package! org-graph-view :recipe (:host github :repo "alphapapa/org-graph-view"))
 (package! org-roam-server)
 ;; Additional Org Packages:7 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Evil][Evil:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Evil][Evil:1]]
 (package! evil-smartparens)
 ;; Evil:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Evil][Evil:2]]
+;; [[file:/tmp/config.org.YCVx5j::*Evil][Evil:2]]
 (package! evil-org)
 ;; Evil:2 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Style][Style:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Style][Style:1]]
+(package! theme-changer)
+;; Style:1 ends here
+
+;; [[file:/tmp/config.org.YCVx5j::*Style][Style:2]]
+;;(package! theme-magic)
+;; Style:2 ends here
+
+;; [[file:/tmp/config.org.YCVx5j::*Style][Style:3]]
 (package! prettify-utils :recipe
                          (:host github
                            :repo "Ilazki/prettify-utils.el"))
-;; Style:1 ends here
+;; Style:3 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Look and Feel and Stuff][Look and Feel and Stuff:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Look%20and%20Feel%20and%20Stuff][Look and Feel and Stuff:1]]
 (package! beacon)
 ;; Look and Feel and Stuff:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Look and Feel and Stuff][Look and Feel and Stuff:2]]
+;; [[file:/tmp/config.org.YCVx5j::*Look%20and%20Feel%20and%20Stuff][Look and Feel and Stuff:2]]
 (package! zoom)
 ;; Look and Feel and Stuff:2 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Writing][Writing:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Writing][Writing:1]]
 (package! flyspell-lazy)
 ;; Writing:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Ansible][Ansible:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Ansible][Ansible:1]]
 (package! jinja2-mode)
 ;; Ansible:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*JS/TS][JS/TS:1]]
+;; [[file:/tmp/config.org.YCVx5j::*JS/TS][JS/TS:1]]
 (package! prettier-js)
 ;; JS/TS:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Administration][Administration:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Administration][Administration:1]]
 (package! counsel-tramp)
 ;; Administration:1 ends here
 
-;; [[file:../../Projekte/dotfiles/.config/doom/config.org::*Fun][Fun:1]]
+;; [[file:/tmp/config.org.YCVx5j::*Fun][Fun:1]]
 (package! xkcd)
 ;; Fun:1 ends here
