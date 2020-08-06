a0579631d8ac00d422744ca11d61a9e89da8db18
valentin_lechner <valentin_lechner@dismail.de>
2020-08-06 17:04:25 +0200

    scripts: in posix sh [[ is undefined

--- .config/local/bin/info-pingrtt.sh
+++ .config/local/bin/info-pingrtt.sh
@@ -15,9 +15,9 @@ else
 	rtt=$(echo "$resp" | grep '=' | tr '/' ' ' | awk '{print $7}' | cut -d '=' -f 2 | head -1)
 
 	ping=${rtt%.*}
-	if [[ "$ping" -lt 50 ]]; then
+	if [ "$ping" -lt 50 ]; then
 		rtt_cl="%{F$(echo "$color5" | cut -d '#' -f 2)}ﯱ ""$rtt ms""%{F444444}"
-	elif [[ "$ping" -lt 150 ]]; then
+	elif [ "$ping" -lt 150 ]; then
 		rtt_cl="%{F$(echo "$color3" | cut -d '#' -f 2)}ﯳ ""$rtt ms""%{F444444}"
 	else
 		rtt_cl="%{F$(echo "$color9" | cut -d '#' -f 2)} ""$rtt ms""%{F444444}"
