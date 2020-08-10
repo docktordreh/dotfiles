566f8b42f76659a611685deb142051ac0a44a10e
valentin_lechner <valentin_lechner@dismail.de>
2020-08-10 16:38:39 +0000

    doom: Adding org-superstar and org-ref

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,82 +1,90 @@
 ;; -*- no-byte-compile: t; -*-
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:1]]
 (package! org-pretty-table-mode
   :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "88380f865a...")
 ;; Additional Org Packages:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:2]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:2]]
 (package! org-super-agenda)
 ;; Additional Org Packages:2 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:3]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:3]]
 (package! doct
   :recipe (:host github :repo "progfolio/doct"))
 ;; Additional Org Packages:3 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:4]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:4]]
 (package! org-fragtog)
 ;; Additional Org Packages:4 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:5]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:5]]
 (package! org-pretty-tags)
 ;; Additional Org Packages:5 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:6]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:6]]
 (package! org-chef)
 ;; Additional Org Packages:6 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Additional%20Org%20Packages][Additional Org Packages:7]]
-(package! org-graph-view :recipe (:host github :repo "alphapapa/org-graph-view"))
-(package! org-roam-server)
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:7]]
+(package! org-ref)
 ;; Additional Org Packages:7 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Evil][Evil:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Additional%20Org%20Packages][Additional Org Packages:8]]
+(package! org-graph-view :recipe (:host github :repo "alphapapa/org-graph-view"))
+(package! org-roam-server)
+;; Additional Org Packages:8 ends here
+
+;; [[file:/tmp/config.org.qGqNYO::*Evil][Evil:1]]
 (package! evil-smartparens)
 ;; Evil:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Evil][Evil:2]]
+;; [[file:/tmp/config.org.qGqNYO::*Evil][Evil:2]]
 (package! evil-org)
 ;; Evil:2 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Style][Style:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Style][Style:1]]
 (package! theme-changer)
 ;; Style:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Style][Style:2]]
+;; [[file:/tmp/config.org.qGqNYO::*Style][Style:2]]
 ;;(package! theme-magic)
 ;; Style:2 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Style][Style:3]]
+;; [[file:/tmp/config.org.qGqNYO::*Style][Style:3]]
 (package! prettify-utils :recipe
                          (:host github
                            :repo "Ilazki/prettify-utils.el"))
 ;; Style:3 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Look%20and%20Feel%20and%20Stuff][Look and Feel and Stuff:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Style][Style:4]]
+(package! org-superstar)
+;; Style:4 ends here
+
+;; [[file:/tmp/config.org.qGqNYO::*Look%20and%20Feel%20and%20Stuff][Look and Feel and Stuff:1]]
 (package! beacon)
 ;; Look and Feel and Stuff:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Look%20and%20Feel%20and%20Stuff][Look and Feel and Stuff:2]]
+;; [[file:/tmp/config.org.qGqNYO::*Look%20and%20Feel%20and%20Stuff][Look and Feel and Stuff:2]]
 (package! zoom)
 ;; Look and Feel and Stuff:2 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Writing][Writing:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Writing][Writing:1]]
 (package! flyspell-lazy)
 ;; Writing:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Ansible][Ansible:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Ansible][Ansible:1]]
 (package! jinja2-mode)
 ;; Ansible:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*JS/TS][JS/TS:1]]
+;; [[file:/tmp/config.org.qGqNYO::*JS/TS][JS/TS:1]]
 (package! prettier-js)
 ;; JS/TS:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Administration][Administration:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Administration][Administration:1]]
 (package! counsel-tramp)
 ;; Administration:1 ends here
 
-;; [[file:/tmp/config.org.Hn3QQU::*Fun][Fun:1]]
+;; [[file:/tmp/config.org.qGqNYO::*Fun][Fun:1]]
 (package! xkcd)
 ;; Fun:1 ends here
